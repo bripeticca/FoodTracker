f791d3a68b178b719205f5483f2e8a0164799f40
Brianna Gemma Peticca <bgpeticca@uwaterloo.ca>
2018-02-19 12:06:46 -0500

    done project

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,9 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by Bri Peticca on 2018-02-05.
+//  Copyright © 2018 Bri Peticca. All rights reserved.
+//
+
+import Foundation
---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Bri Peticca on 2018-01-12.
+//  Copyright © 2018 Bri Peticca. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
